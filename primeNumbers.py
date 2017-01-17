ac5b07d00865131dd306e247477aa75f9b950a70
Wanjiru Ndung'u <daisy@localhost.localdomain>
2017-01-17 16:24:17 +0300

    Prime Numbers

---
+++ primeNumbers.py
@@ -0,0 +1,8 @@
+x= input ("Enter any number to generate Prime Numbers to: ")
+if x > 1:
+	for num in range(0, x):
+
+		for factor in range(0, num):
+			if (num % 2 and num % 3) == 0: #a number is said to be non prime if the number modulus 2 and 3 are both equal to zero
+			 break
+			
