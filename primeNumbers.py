f0dfa9c01fc6dabcd9939d7f77ca118a010f024e
Wanjiru Ndung'u <daisy@localhost.localdomain>
2017-01-17 16:25:43 +0300

    Prime Numbers

--- primeNumbers.py
+++ primeNumbers.py
@@ -5,4 +5,7 @@ if x > 1:
 		for factor in range(0, num):
 			if (num % 2 and num % 3) == 0: #a number is said to be non prime if the number modulus 2 and 3 are both equal to zero
 			 break
-			
+			else:
+			 print(num) #prints prime numbers between 0->x
+	else:
+	 print ("Invalid output")
